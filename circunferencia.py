0fb1038e9570ac3bbaa196667279f4e6c0f874c4
JoaquinEspinoza <joakonator@gmail.com>
2023-04-14 21:20:07 -0400

    commit3

---
+++ circunferencia.py
@@ -0,0 +1,10 @@
+import math
+
+# Prompt for radius
+radius = float(input("Radio: "))
+
+# Calculate circumference
+circumference = 2 * 3.14159 * radius
+
+# Print results
+print("La  circunferencia del circulo es:", circumference)
